aa18097d2610f5a7b38a668d2cf091bfa2a7e4db
Keith Prince <keithcprince@gmail.com>
2019-09-30 16:26:10 -0400

    annogram

--- drill1.js
+++ drill1.js
@@ -237,3 +237,23 @@ function isBinary(n){
 
 console.log(isBinary(3));
 console.log(isBinary(25));
+
+
+function allanagrams(string){
+
+  if(string.length === 0) return [''];
+    var result = {};
+    string.split('').forEach(function(letter,i){
+          var remainingLetters = string.slice(0,i) + string.slice(i+1);
+ 
+           allanagrams(remainingLetters).forEach(
+               function(anagram){
+           result[letter+anagram] = true;
+ });
+ });
+        return Object.keys(result);
+        
+ }
+ 
+ var str = 'east';
+ console.log(allanagrams(str));
